69df85ff56ed0b16447430c111eda2613ea8f768
mgv <mgv@akait.dk>
2022-09-19 17:47:43 +0200

    Transport Pris Beregner class and tests

---
+++ Transportpris/Tests/PrisTester.cs
@@ -0,0 +1,27 @@
+﻿using FluentAssertions;
+using Transportpris.Transportberegner;
+using Xunit;
+
+namespace Transportpris.Tests;
+
+public class PrisTester
+{
+    [Theory]
+    [InlineData(4, 9, 0)]
+    [InlineData(4, 15, 50)]
+    [InlineData(6, 9, 75)]
+    [InlineData(6, 30, 100)]
+    [InlineData(5, 10, 100)]
+    public void DifferentDistancesAndWeightsReturnDifferentPrices(int distance, int weight, int expectedResult)
+    {
+        //Arrange
+        var prisBeregner = new Pris();
+        
+        //Act
+        var pris = prisBeregner.pris(distance, weight);
+        
+        //Assert
+        pris.Should().Be(expectedResult);
+    }
+
+}
---
+++ Transportpris/Transportberegner/Pris.cs
@@ -0,0 +1,25 @@
+﻿namespace Transportpris.Transportberegner;
+
+public class Pris
+{
+    public int pris(int afstand, int vaegt)
+    {
+        var pris = 0;
+        switch ((afstand, vaegt))
+        {
+            case (<5, <10):
+                break;
+            case(<5,>=10):
+                pris = 50;
+                break;
+            case(>=5, <10):
+                pris = 75;
+                break;
+            default:
+                pris = 100;
+                break;
+        }
+
+        return pris;
+    }
+}
